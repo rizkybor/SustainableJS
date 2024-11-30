aeb8d555a6cb146a908a17a94a5a549c3c8a1f61
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2024-11-30 15:33:44 +0700

    update check middleware

--- jury-section-app/src/middleware.ts
+++ jury-section-app/src/middleware.ts
@@ -1,3 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export default function middleware(req: NextRequest) {
+  console.log("Middleware invoked for:", req.nextUrl.pathname);
+  return NextResponse.next();
+}
+
 export const config = {
-    matcher: [], // Disable middleware
-  };
+  matcher: ['/dashboard/:path*'],
+};

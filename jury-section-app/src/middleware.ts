aaff11b31277e0008bca089eddfdf636c3d2e640
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2024-11-30 17:06:32 +0700

    update

--- jury-section-app/src/middleware.ts
+++ jury-section-app/src/middleware.ts
@@ -1,12 +1,5 @@
-import { NextResponse } from "next/server";
-
-export function middleware(request: Request) {
-  const response = NextResponse.next();
-  response.headers.set("Access-Control-Allow-Origin", "*"); // Sesuaikan domain Anda di sini
-  response.headers.set("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  return response;
-}
+export {default} from "next-auth/middleware"
 
 export const config = {
-  matcher: "/api/:path*",
-};
+    matcher: ['/dashboard/:path*']
+}

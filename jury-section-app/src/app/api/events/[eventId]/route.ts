bf05a309ee3f52d18a608a799449e407caf487f4
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2024-12-02 19:48:08 +0700

    update-get-event-by-id

--- jury-section-app/src/app/api/events/[eventId]/route.ts
+++ jury-section-app/src/app/api/events/[eventId]/route.ts
@@ -1,33 +1,26 @@
 import { connectToMongoDB } from "@/lib/mongodb";
-import mongoose from "mongoose";
 import { NextResponse } from "next/server";
+import mongoose from "mongoose";
 import { ObjectId } from "mongodb";
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
+export async function GET(request: Request, context: any) {
   try {
+    const {params} = context
+    const id = params.eventId
     // Hubungkan ke MongoDB
     await connectToMongoDB();
-    
-    // Ambil ID dari `params` (dynamic route)
-    const id = params.id;
-    console.log(id,'<< cek id dari params')
 
-    if (!id) {
-      return NextResponse.json(
-        { error: "ID is required" },
-        { status: 400 } // Bad Request
-      );
-    }
-
-    // Periksa koneksi
     if (!mongoose.connection.db) {
       throw new Error("MongoDB connection does not have a valid `db` object.");
     }
 
-    // Query berdasarkan ID
+    let objectId: ObjectId;
+    objectId = new ObjectId(id);
+
+    // // Query data berdasarkan ID
     const event = await mongoose.connection.db
       .collection("eventsCollection")
-      .findOne({ _id: new ObjectId(id) });
+      .findOne({ _id: objectId });
 
     if (!event) {
       return NextResponse.json(
@@ -36,13 +29,16 @@ export async function GET(req: Request, { params }: { params: { id: string } }) 
       );
     }
 
-    // Kembalikan data sebagai respons
+    // Kembalikan data sebagai JSON
     return NextResponse.json(event);
   } catch (error) {
     console.error("Error fetching event by ID:", error);
     return NextResponse.json(
-      { error: error instanceof Error ? error.message : "An unknown error occurred" },
-      { status: 500 }
+      {
+        error:
+          error instanceof Error ? error.message : "An unknown error occurred",
+      },
+      { status: 500 } // Internal Server Error
     );
   }
 }
